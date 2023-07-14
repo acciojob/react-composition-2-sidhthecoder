44dbaba8ea5c36538e0f034addcc303c3b914aab
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-07-14 17:25:26 +0530

    submitting code-Fri Jul 14 2023 17:25:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 import Modal from './Modal';
 
-const App = () => {
+function App() {
   const [showModal, setShowModal] = useState(false);
 
   const handleCloseModal = () => {
     setShowModal(false);
   };
 
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
   return (
     <div>
-      <button onClick={() => setShowModal(true)}>Show Modal</button>
-      <Modal show={showModal} onClose={handleCloseModal}/>
-       
-      
+      <button onClick={handleOpenModal}>Open Modal</button>
+      <Modal show={showModal} onClose={handleCloseModal}>
+        <h1>Show Modal</h1>
+        <p>This is the content of the modal.</p>
+      </Modal>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import '../styles/App.css'
-const Modal = ({ show, onClose}) => {
-  
+
+function Modal({ show, onClose, children }) {
+  const handleClose = () => {
+    onClose();
+  };
 
   if (!show) {
     return null;
@@ -9,12 +11,14 @@ const Modal = ({ show, onClose}) => {
 
   return (
     <div className="modal-overlay">
-      <button className="modal-close" onClick={onClose}>
-        Close
-      </button>
-      <p>This is the content of the modal.</p>
+      <div className="modal-dialog">
+        <button className="modal-close" onClick={handleClose}>
+          Close
+        </button>
+        {children}
+      </div>
     </div>
   );
-};
+}
 
 export default Modal;
